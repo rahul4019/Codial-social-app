8a46e69d79f132e5d4deacc07a36aaae0eddff5e
Rahul <rahulkashyap4019@gmail.com>
2022-09-28 07:18:59 +0530

    Add login component and style it

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { getPosts } from '../api';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -27,8 +40,16 @@ function App() {
   }
   return (
     <div className="App">
-      <Navbar />
-      <Home posts={posts} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home posts={posts} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user/asdas" element={<UserInfo />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import styles from '../styles/login.module.css';
+import { useToasts } from 'react-toast-notifications';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const { addToast } = useToasts();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoggingIn(true);
+
+    if (!email || !password) {
+      return addToast('Please enter both email and Password', {
+        appearance: 'error',
+      });
+    }
+  };
+
+  return (
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
+      <span className={styles.loginSignupHeader}>Log In</span>
+      <div className={styles.field}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className={styles.field}>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className={styles.field} disabled={loggingIn}>
+        <button>{loggingIn ? 'Logging in...' : 'Log In'}</button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
