3bbd7d48e65c8ed7d6240908702d70ad8cad66ba
Rahul <rahulkashyap4019@gmail.com>
2022-10-01 09:54:23 +0530

    Implement logic to create post and display it on home page

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,11 +1,37 @@
 import { useState } from 'react';
+import { useToasts } from 'react-toast-notifications';
+
 import styles from '../styles/home.module.css';
+import { addPost } from '../api';
+import { usePosts } from '../hooks';
 
 const CreatePost = () => {
   const [post, setPost] = useState('');
   const [addingPost, setAddingPost] = useState(false);
+  const { addToast } = useToasts();
+  const posts = usePosts()
 
-  const handleAddPostClick = () => {};
+  const handleAddPostClick = async () => {
+    setAddingPost(true);
+
+    // do some checks
+    const response = await addPost(post);
+
+    if (response.success) {
+      setPost('');
+      posts.addPostToState(response.data.post)
+
+      addToast('Post created successfully', {
+        appearance: 'success',
+      });
+    } else {
+      addToast(response.message, {
+        appearance: 'error',
+      });
+    }
+    
+    setAddingPost(false);
+  };
 
   return (
     <div className={styles.createPost}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,12 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import { Comment, CreatePost, FriendsList, Loader } from '../components';
-import { getPosts } from '../api';
+import { Post, CreatePost, FriendsList, Loader } from '../components';
 import styles from '../styles/home.module.css';
-import { useAuth } from '../hooks';
+import { useAuth, usePosts } from '../hooks';
 
 const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState([]);
   const auth = useAuth();
+  const posts = usePosts();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await getPosts();
-
-      if (response.success) {
-        setPosts(response.data.posts);
-      }
-
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (loading) {
+  if (posts.loading) {
     return <Loader />;
   }
 
@@ -33,55 +14,8 @@ const Home = () => {
     <div className={styles.home}>
       <div className={styles.postsList}>
         <CreatePost />
-        {posts.map((post) => (
-          <div className={styles.postWrapper} key={`post-${post._id}`}>
-            <div className={styles.postHeader}>
-              <div className={styles.postAvatar}>
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/1144/1144709.png"
-                  alt="user-pic"
-                />
-                <div>
-                  <Link
-                    to={`/user/${post.user._id}`}
-                    state={{ user: post.user }}
-                    className={styles.postAuthor}
-                  >
-                    {post.user.name}
-                  </Link>
-                  <span className={styles.postTime}>a minute ago</span>
-                </div>
-              </div>
-              <div className={styles.postContent}>{post.content}</div>
-
-              <div className={styles.postActions}>
-                <div className={styles.postLike}>
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/889/889140.png"
-                    alt="likes-icon"
-                  />
-                  <span>5</span>
-                </div>
-
-                <div className={styles.postCommentsIcon}>
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/3193/3193015.png"
-                    alt="comments-icon"
-                  />
-                  <span>{post.comments.length}</span>
-                </div>
-              </div>
-              <div className={styles.postCommentBox}>
-                <input placeholder="Start typing a comment" />
-              </div>
-
-              <div className={styles.postCommentsList}>
-                {post.comments.map((comment) => (
-                  <Comment comment={comment} />
-                ))}
-              </div>
-            </div>
-          </div>
+        {posts.data.map((post) => (
+          <Post post={post} key={`post-${post._id}`} />
         ))}
       </div>
       {auth.user && <FriendsList />}
